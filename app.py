bf3c421fb8cb1f64bafd9203dede93da0a1def67
trixTRr <nullentries@yandex.ru>
2023-12-21 17:35:47 +0300

    fix: added notifications

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 import psycopg2
 from scanner import Scanner
 
@@ -43,20 +43,28 @@ def index():
 @app.route('/create_database', methods=['GET', 'POST'], endpoint='create_db')
 @check_db_not_exists
 def create_db():
+    if request.method == 'GET':
+        return render_template('create_db.html')
     if request.method == 'POST':
         status = scanner.create_database()
-        return redirect(url_for('index'))
-    return render_template('create_db.html')
+        if status:
+            return jsonify({"status": True, "message": "База данных создана"})
+        else:
+            return jsonify({"status": False, "message": "База данных не создана"})
 
 
 # Database deletion page.
 @app.route('/delete_db', methods=['GET', 'POST'], endpoint='delete_db')
 @check_db_exists
 def delete_db():
+    if request.method == 'GET':
+        return render_template('delete_db.html')
     if request.method == 'POST':
-        status = scanner.drop_database()
-        return redirect(url_for('index'))
-    return render_template('delete_db.html')
+        status = scanner.create_database()
+        if status:
+            return jsonify({"status": True, "message": "База данных удалена"})
+        else:
+            return jsonify({"status": False, "message": "База данных не удалена"})
 
 
 # Search by IP-address page.
@@ -64,15 +72,20 @@ def delete_db():
 @check_db_exists
 def ip_search():
     message = None
+    if request.method == 'GET':
+        return render_template('ip_search.html', message=message)
     if request.method == 'POST':
         ip_address = request.form['ipAddress']
         
         filtr = Scanner.select_filter()
         filtr.add_ip_to_filter(ip_address)
 
-        results = scanner.get_filtered_summary(filtr)
+        status = scanner.get_filtered_summary(filtr)
 
-    return render_template('ip_search.html', message=message)
+    if status:
+        return jsonify({"status": True, "message": "IP-адрес найден"})
+    else:
+        return jsonify({"status": False, "message": "IP-адрес не найден"})
 
 
 # Search by filters page.
@@ -84,15 +97,19 @@ def filter_search():
         filtr = Scanner.select_filter()
 
         onion_routing = request.form.get('tor')
-        status = filtr.set_onion_routing_filter(onion_routing == 'on')
+        filtr.set_onion_routing_filter(onion_routing == 'on')
 
         port = request.form.get('port')
         if port:
-            status = filtr.add_port_to_filter(int(port))
+            filtr.add_port_to_filter(int(port))
         
         ip_addr = request.form.get('ipAddress')
         if ip_addr:
-            status = filtr.add_ip_to_filter(ip_addr)
+            filtr.add_ip_to_filter(ip_addr)
+
+        country = request.form.get('country')
+        if ip_addr:
+            filtr.add_country_to_filter(country)
 
         results = scanner.get_filtered_summary(filtr)
 
@@ -100,39 +117,45 @@ def filter_search():
 
 
 # Function for deleting rows in filter_search page.
-@app.route('/delete_record', methods=['GET', 'POST'], endpoint='delete_record')
+@app.route('/delete_record', methods=['POST'], endpoint='delete_record')
 def delete_record():
-    ip_addr = request.form['ip_addr']
-    status = scanner.delete_host(ip_addr)
-
-    
+    if request.method == 'POST':
+        ip_addr = request.form.get('ip_addr')
+        # status = scanner.delete_host(ip_addr)
+        status = True
+        if status:
+            return jsonify({"status": True, "message": "Запись удалена"})
+        else:
+            return jsonify({"status": False, "message": "Запись не удалена"})
 
 
 # Clear tables page.
 @app.route('/clear_tables', methods=['GET', 'POST'], endpoint='clear_tables')
 @check_db_exists
 def clear_tables():
-
-    return render_template('clear_tables.html')
+    if request.method == 'GET':
+        return render_template('clear_tables.html')
+    if request.method == 'POST':
+        status = scanner.clear_tables()
+        if status:
+            return jsonify({"status": True, "message": "Таблицы удалены"})
+        else:
+            return jsonify({"status": False, "message": "Таблицы не удалены"})
 
 
 # Clear IP-address page.
 @app.route('/clear_ip', methods=['GET', 'POST'], endpoint='clear_ip')
 @check_db_exists
 def clear_ip():
+    if request.method == 'GET':
+        return render_template('clear_ip.html')
     if request.method == 'POST':
         ip_address = request.form['ipAddress']
-        
         status = scanner.delete_host(ip_address)
-
         if status:
-            message = f"Успешно удален '{ip_address}'"
+            return jsonify({"status": True, "message": "IP-адрес удален"})
         else:
-            message = f"Произошла ошибка при удалении '{ip_address}'"
-
-        return render_template('clear_ip.html', message=message)
-    else:
-        return render_template('clear_ip.html')
+            return jsonify({"status": False, "message": "IP-адрес не удален"})
 
 
 if __name__ == '__main__':
